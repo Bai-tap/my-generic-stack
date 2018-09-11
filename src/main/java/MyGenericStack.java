2b18a3e4079282eaf039ca72d7170cbcdaa7e499
longhoang94 <longsutls@gmail.com>
2018-09-11 11:43:48 +0700

    add class MyGenericStack

---
+++ src/main/java/MyGenericStack.java
@@ -0,0 +1,31 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        } else return false;
+    }
+
+    public int size() {
+        return stack.size();
+    }
+}
